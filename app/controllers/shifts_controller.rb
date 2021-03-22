eb9cdec7038d76c4c355741632acc017a0df791c
Gabrielle <eastongabrielle@gmail.com>
2021-03-22 10:56:25 -0400

    updates shift controller set method

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -56,10 +56,15 @@ class ShiftsController < ApplicationController
     end
   end
 
+  def render_not_found
+    render :file => "#{RAILS_ROOT}/public/404.html",  :status => 404
+  end
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_shift
       @shift = Shift.find(params[:id])
+      return render_not_found unless @shift
     end
 
     # Only allow a list of trusted parameters through.
